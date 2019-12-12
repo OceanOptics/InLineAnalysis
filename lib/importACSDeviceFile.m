b7f34cd01bf7c3adef60f9c9069251c70372f359
GuiBourdin <50623880+guibourdin@users.noreply.github.com>
2019-12-12 12:49:36 -0500

    Update importACSDeviceFile.m

--- lib/importACSDeviceFile.m
+++ lib/importACSDeviceFile.m
@@ -2,24 +2,30 @@ function [lambda_c, lambda_a, n] = importACSDeviceFile(filename, verbose)
 % Get wavelength out of ACS device file
 
 fid=fopen(filename);
-lambda_c = [];
-lambda_a = [];
-n = NaN; i = 1;
-while ~feof(fid)
-    l = fgetl(fid);
-    if contains(l, 'number of temperature bins')
-        % Init arrays with number of wavelength
-        foo = strsplit(l, ';');
-        n = str2double(foo{1});
-        lambda_c = NaN(1,n);
-        lambda_a = NaN(1,n);
-    elseif l(1) == 'C'
-        % Get wavelength at each line
-        foo = strsplit(l, '\t\t');
-        bar = strsplit(foo{1}, '\t');
-        lambda_c(i) = str2double(bar{1}(2:end));
-        lambda_a(i) = str2double(bar{2}(2:end));
-        i = i + 1;
+
+if contains(filename,{'ac9','AC9'})
+    lambda_c = [412 440 488 510 532 555 650 676 715];
+    lambda_a = lambda_c;
+else
+    lambda_c = [];
+    lambda_a = [];
+    n = NaN; i = 1;
+    while ~feof(fid)
+        l = fgetl(fid);
+        if contains(l, 'number of temperature bins')
+            % Init arrays with number of wavelength
+            foo = strsplit(l, ';');
+            n = str2double(foo{1});
+            lambda_c = NaN(1,n);
+            lambda_a = NaN(1,n);
+        elseif l(1) == 'C'
+            % Get wavelength at each line
+            foo = strsplit(l, '\t\t');
+            bar = strsplit(foo{1}, '\t');
+            lambda_c(i) = str2double(bar{1}(2:end));
+            lambda_a(i) = str2double(bar{2}(2:end));
+            i = i + 1;
+        end
     end
 end
 
